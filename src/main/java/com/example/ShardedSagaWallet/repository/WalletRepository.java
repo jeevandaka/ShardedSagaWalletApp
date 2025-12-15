b093747027ed7c7369e0ab04445ac99d6058a87d
Jeevan Daka <jeevandaka@gmail.com>
2025-12-15 11:38:46 +0530

    bug fixed + fixed issue of sharding by adding sharding strategy for tables into shardin.yml

--- src/main/java/com/example/ShardedSagaWallet/repository/WalletRepository.java
+++ src/main/java/com/example/ShardedSagaWallet/repository/WalletRepository.java
@@ -5,6 +5,7 @@ import com.example.ShardedSagaWallet.saga.SagaContext;
 import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -20,7 +21,9 @@ public interface WalletRepository extends JpaRepository<Wallet,Long> {
     @Query("SELECT w FROM Wallet w WHERE w.userId = :id")
     Optional<Wallet> findByIdWithLock(@Param("id") Long id);
 
-    void updateBalanceByUserId(Long userId, BigDecimal add);
+    @Modifying
+    @Query("UPDATE Wallet w SET w.balance = :balance WHERE w.userId = :userId")
+    void updateBalanceByUserId(@Param("userId") Long userId, @Param("balance") BigDecimal balance);
 
     List<Wallet> findByUserId(Long userId);
 }
